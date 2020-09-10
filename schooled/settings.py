09dbade74c45d92ee0756986892a2b81378f71f7
Davo <davidakinfenwa2@gmail.com>
2020-09-10 14:03:49 +0100

    Some slight fixes made

--- schooled/settings.py
+++ schooled/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'schooledcontact@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWrRD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
