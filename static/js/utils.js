9d1fd02dd7188296356440acd28802f833b0eb4f
Davo <davidakinfenwa2@gmail.com>
2023-01-21 16:51:53 +0100

    ...

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,36 +1,61 @@
-//Scrolls to element with scrolling animation
-function ScrollToId(id,backwards=50) {
-	console.log(backwards)
-	console.log(document.querySelector(id).offsetTop)
+//Scrolls to elements 
+function ScrollToId(id, backwards = 70) {
 	window.location.href = id;
-
 	let body = document.querySelectorAll("html,body");
-	body.forEach(function(elem) {
+	body.forEach(function (elem) {
 		elem.scrollTop = document.querySelector(id).offsetTop - backwards;
 	});
 }
 
-function ScrollToMain() {
-	let body = document.querySelector("html");
-	body.scrollTop = 450;
-}
-
-function animateScrollTo(selector) {
+function ScrollTo(selector) {
 	let body = document.querySelectorAll("html,body");
-	body.forEach(function(elem) {
-		elem.scrollTop = document.querySelector(selector).offsetTop - 50;
+	body.forEach(function (elem) {
+		elem.scrollTop = document.querySelector(selector).offsetTop - 70;
 	});
 }
 
+
+//Marks an active list
 let block_list = document.querySelectorAll(".block-list li")
 block_list.forEach(function (elem) {
-	elem.onclick = function (e) {
-		for(let i=0; i<block_list.length;i++){
+	elem.addEventListener("click", function (e) {
+		for (let i = 0; i < block_list.length; i++) {
 			block_list[i].classList.remove("active");
 		}
 		elem.classList.add("active")
-	}
+	});
 });
+
+let tools_list = document.querySelectorAll("#toolsList li")
+tools_list.forEach(function (elem) {
+	elem.addEventListener("click", function (e) {
+		for (let i = 0; i < tools_list.length; i++) {
+			let last_child = tools_list[i].lastElementChild;
+			last_child.classList.replace("badge-warning","badge-info");
+			last_child.textContent = "Show";
+		}
+		let last_child = elem.lastElementChild;
+		last_child.classList.replace("badge-info","badge-warning");
+		last_child.textContent = "Hide";
+	});
+});
+
+document.querySelectorAll(".block-list li,#toolsList li").forEach(function (element) {
+	element.addEventListener("htmx:sendError", function (event) {
+		let triggeredElement = event.target;
+		let target_selector = triggeredElement.getAttribute("hx-target");
+		let target = document.querySelector(target_selector);
+		target.innerHTML = '<div class="d-flex flex-column h-100 justify-content-center"><svg class="mx-auto" width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.23 7.913 7.91 3.23c.15-.15.35-.23.57-.23h7.05c.21 0 .42.08.57.23l4.67 4.673c.15.15.23.35.23.57v7.054c0 .21-.08.42-.23.57L16.1 20.77c-.15.15-.35.23-.57.23H8.47a.81.81 0 0 1-.57-.23l-4.67-4.673a.793.793 0 0 1-.23-.57V8.473c0-.21.08-.42.23-.57v.01Z" fill="#000000" fill-opacity=".16" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linejoin="round"/><path d="M12 16h.008M12 8v5" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/></svg><small class="mx-auto text-danger">Something went wrong.</div>';
+	});
+
+	element.addEventListener("htmx:xhr:loadstart", function (event, e) {
+		let triggeredElement = event.target;
+		let target_selector = triggeredElement.getAttribute("hx-target");
+		let target = document.querySelector(target_selector);
+		target.innerHTML = '<div class="m-auto d-flex flex-column"><div class="mx-auto progressBar"></div><small class="text-center">Fetching requested items</small></div>';
+	});
+
+})
 
 //make navbar transparent when user scrolls down.
 var main_nav = document.getElementById("main-nav");
@@ -39,108 +64,108 @@ window.onscroll = function () {
 	main_nav.style.opacity = "0.5";
 }
 
-function write_alert(msg,type){
-	if(type===undefined){type="success"}
-        if(type=="danger"){
-            type_m = "Error";
-        }else{
-            type_m = type[0].toUpperCase() + type.substr(1);
-        }
+function write_alert(msg, type) {
+	if (type === undefined) { type = "success" }
+	if (type == "danger") {
+		type_m = "Error";
+	} else {
+		type_m = type[0].toUpperCase() + type.substr(1);
+	}
 
-        return '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert"><strong>' + type_m + '!</strong> ' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+	return '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert"><strong>' + type_m + '!</strong> ' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 }
 
-function write_loader(msg){
-	if(msg===undefined){msg=""}
-    return '<div class="m-auto d-flex flex-column"><div class="mx-auto progressBar"></div><small class="mx-auto">'+msg+'</small></div>';
+function write_loader(msg) {
+	if (msg === undefined) { msg = "" }
+	return '<div class="m-auto d-flex flex-column"><div class="mx-auto progressBar"></div><small class="mx-auto">' + msg + '</small></div>';
 }
 //Help section
 var help_requested = false;
 function toggleHelp(show) {
-	if(show){
+	if (show) {
 		$("#help-container").removeClass("close");
-        setTimeout(function(){
-		document.getElementById("main-page-body").style.display = "none";
-        },500);
+		setTimeout(function () {
+			document.getElementById("main-page-body").style.display = "none";
+		}, 500);
 		var body = document.getElementById("help-container-body");
-		if(!help_requested){
-			get_data("/accounts/help_page/",{},body);
-            help_requested = true;
+		if (!help_requested) {
+			get_data("/accounts/help_page/", {}, body);
+			help_requested = true;
 		}
-	}else{
+	} else {
 		$("#help-container").addClass("close");
 		document.getElementById("main-page-body").style.display = "block";
 	}
 }
 var list;
-$(document).click(function(event) {
-	if(!list){return;}
-    var $target = $(event.target);
-    if(!$target.closest('#helpSideNav').length && $('#helpSideNav').is(":visible") && (event.target !== list[0]) ) {
-        document.getElementById("helpSideNav").style.width = "0";
-        $("#closeHelp")[0].disabled = false;
-    }        
+$(document).click(function (event) {
+	if (!list) { return; }
+	var $target = $(event.target);
+	if (!$target.closest('#helpSideNav').length && $('#helpSideNav').is(":visible") && (event.target !== list[0])) {
+		document.getElementById("helpSideNav").style.width = "0";
+		$("#closeHelp")[0].disabled = false;
+	}
 });
-	
+
 //End help section
-function check_file(input){
-	if(input.files[0]){
+function check_file(input) {
+	if (input.files[0]) {
 		return true;
-	}else{
+	} else {
 		return false;
 	}
 }
 
-function post_data(url,data,elem,use_server_msg){
-	if(use_server_msg===undefined){use_server_msg=false}
+function post_data(url, data, elem, use_server_msg) {
+	if (use_server_msg === undefined) { use_server_msg = false }
 	//Add loader
-	var elem_obj =$(elem);
+	var elem_obj = $(elem);
 	elem_obj.html(write_loader("Submitting..."));
-	$.post(url,data).done(function(data){
-        if(use_server_msg){
-            if(data){
-		        $(elem).html(write_alert(data));
-                return;
-            }
-        }
+	$.post(url, data).done(function (data) {
+		if (use_server_msg) {
+			if (data) {
+				$(elem).html(write_alert(data));
+				return;
+			}
+		}
 		$(elem).html(write_alert("Data was sent successfully"));
 
-	}).fail(function(res,textStatus,errorThrown){
-        if(use_server_msg){
-            if(res.responseText){
-		        $(elem).html(write_alert(res.responseText,"danger"));
-                return;
-            }
-        }
-		$(elem).html(write_alert("Something went wrong, request was not successful.","danger"));
+	}).fail(function (res, textStatus, errorThrown) {
+		if (use_server_msg) {
+			if (res.responseText) {
+				$(elem).html(write_alert(res.responseText, "danger"));
+				return;
+			}
+		}
+		$(elem).html(write_alert("Something went wrong, request was not successful.", "danger"));
 	});
 }
 
-function send_request(url,type,data,func){
+function send_request(url, type, data, func) {
 	$.ajax({
 		url: url,
 		type: type,
 		timeout: 20000,
 		data: data,
-		success: function(data,textStatus,res){
-			func(res,"SUCCESS");
+		success: function (data, textStatus, res) {
+			func(res, "SUCCESS");
 		},
-		error: function(res,textStatus,errorThrown){
-			func(res,"ERROR");
+		error: function (res, textStatus, errorThrown) {
+			func(res, "ERROR");
 		},
-		complete: function(res,textStatus){
-			func(res,"COMPLETE");
+		complete: function (res, textStatus) {
+			func(res, "COMPLETE");
 		}
 	});
 }
 
-function post_form(url,form,status_elem,use_server_msg){
-	if(use_server_msg===undefined){use_server_msg=false}
+function post_form(url, form, status_elem, use_server_msg) {
+	if (use_server_msg === undefined) { use_server_msg = false }
 	var $form = $(form);
-	if(status_elem===undefined){
+	if (status_elem === undefined) {
 		$form.find("#form-status").remove();
-        status_elem = document.createElement("div");
-        status_elem.className = "my-3";
+		status_elem = document.createElement("div");
+		status_elem.className = "my-3";
 		status_elem.id = "form-status";
 		$form.append(status_elem);
 	}
@@ -152,34 +177,34 @@ function post_form(url,form,status_elem,use_server_msg){
 		type: "Post",
 		timeout: 15000,
 		data: $form.serialize(),
-		success: function(data){
-			if(use_server_msg){
-				if(data){
+		success: function (data) {
+			if (use_server_msg) {
+				if (data) {
 					status_elem.html(write_alert(data));
 					return;
 				}
 			}
 			status_elem.html(write_alert("Form was submitted successfully"));
 		},
-		error: function(res,textStatus,errorThrown){
-			if(use_server_msg){
-				if(res.responseText){
-					status_elem.html(write_alert(res.responseText,"danger"));
+		error: function (res, textStatus, errorThrown) {
+			if (use_server_msg) {
+				if (res.responseText) {
+					status_elem.html(write_alert(res.responseText, "danger"));
 					return;
 				}
 			}
-			status_elem.html(write_alert("Something went wrong, form was not submitted.","danger"));
+			status_elem.html(write_alert("Something went wrong, form was not submitted.", "danger"));
 		}
 	});
 }
 
-function post_form_file(url,form,status_elem,use_server_msg){
-	if(use_server_msg===undefined){use_server_msg=false}
+function post_form_file(url, form, status_elem, use_server_msg) {
+	if (use_server_msg === undefined) { use_server_msg = false }
 	var $form = $(form);
-	if(status_elem===undefined){
+	if (status_elem === undefined) {
 		$form.find("#form-status").remove();
-        status_elem = document.createElement("div");
-        status_elem.className = "my-3";
+		status_elem = document.createElement("div");
+		status_elem.className = "my-3";
 		status_elem.id = "form-status";
 		$form.append(status_elem);
 	}
@@ -193,47 +218,48 @@ function post_form_file(url,form,status_elem,use_server_msg){
 		contentType: false,
 		processData: false,
 		data: formdata,
-		success: function(data){
-			if(use_server_msg){
-				if(data){
+		success: function (data) {
+			if (use_server_msg) {
+				if (data) {
 					status_elem.html(write_alert(data));
 					return;
 				}
 			}
 			status_elem.html(write_alert("Form was submitted successfully"));
 		},
-		error: function(res,textStatus,errorThrown){
-			if(use_server_msg){
-				if(res.responseText){
-					status_elem.html(write_alert(res.responseText,"danger"));
+		error: function (res, textStatus, errorThrown) {
+			if (use_server_msg) {
+				if (res.responseText) {
+					status_elem.html(write_alert(res.responseText, "danger"));
 					return;
 				}
 			}
-			status_elem.html(write_alert("Something went wrong, form was not submitted.","danger"));
+			status_elem.html(write_alert("Something went wrong, form was not submitted.", "danger"));
 		}
 	});
 }
 
-function get_data(url,data,elem,status_elem,use_server_msg){
-	if(use_server_msg===undefined){use_server_msg=false}
-	if(data===undefined){data={}}
+function get_data(url, data, elem, status_elem, use_server_msg) {
+	if (use_server_msg === undefined) { use_server_msg = false }
+	if (data === undefined) { data = {} }
 
 	//Add loader
-    if(status_elem!==undefined){
-	    var status_obj = $(status_elem);
-    }else{
-	    var status_obj =$(elem);
-    }
+	if (status_elem !== undefined) {
+		var status_obj = $(status_elem);
+	} else {
+		var status_obj = $(elem);
+	}
 	status_obj.html(write_loader());
-	$.get(url,data).done(function(data){
+	$.get(url, data).done(function (data) {
 		status_obj.html("");
-        $(elem).html(data);
-	}).fail(function(res,textStatus,errorThrown){if(use_server_msg){
-		if(res.responseText){
-			status_obj.html(write_alert(res.responseText,"danger"));
-			return;
+		$(elem).html(data);
+	}).fail(function (res, textStatus, errorThrown) {
+		if (use_server_msg) {
+			if (res.responseText) {
+				status_obj.html(write_alert(res.responseText, "danger"));
+				return;
 			}
 		}
-		status_obj.html(write_alert("Something went wrong, request was not successful.","danger"));
+		status_obj.html(write_alert("Something went wrong, request was not successful.", "danger"));
 	});
 }
