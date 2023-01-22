802d23b570ff76b01178da9ed29abd298a91f058
Davo <davidakinfenwa2@gmail.com>
2023-01-22 18:45:02 +0100

    ...

--- static/js/utils.js
+++ static/js/utils.js
@@ -31,32 +31,16 @@ tools_list.forEach(function (elem) {
 	elem.addEventListener("click", function (e) {
 		for (let i = 0; i < tools_list.length; i++) {
 			let last_child = tools_list[i].lastElementChild;
-			last_child.classList.replace("badge-warning","badge-info");
+			last_child.classList.replace("badge-warning", "badge-info");
 			last_child.textContent = "Show";
 		}
 		let last_child = elem.lastElementChild;
-		last_child.classList.replace("badge-info","badge-warning");
+		last_child.classList.replace("badge-info", "badge-warning");
 		last_child.textContent = "Hide";
 	});
 });
 
-document.addEventListener("htmx:beforeSend", function (event) {
-	detail = event.detail;
-	let target = detail.target;
-	target.innerHTML = '<div class="m-auto d-flex flex-column"><div class="mx-auto progressBar"></div><small class="text-center">Processing...</small></div>';
-});
 
-document.addEventListener("htmx:responseError", function (event) {
-	detail = event.detail;
-	target = detail.target;
-	target.innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Sorry!</strong>  But your request could not be completed. <hr class="mb-0">' + detail.xhr.responseText + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-
-});
-
-document.addEventListener("htmx:sendError", function (event) {
-		let target = event.detail.target;
-		target.innerHTML = '<div class="d-flex flex-column h-100 justify-content-center"><svg class="mx-auto" width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.23 7.913 7.91 3.23c.15-.15.35-.23.57-.23h7.05c.21 0 .42.08.57.23l4.67 4.673c.15.15.23.35.23.57v7.054c0 .21-.08.42-.23.57L16.1 20.77c-.15.15-.35.23-.57.23H8.47a.81.81 0 0 1-.57-.23l-4.67-4.673a.793.793 0 0 1-.23-.57V8.473c0-.21.08-.42.23-.57v.01Z" fill="#000000" fill-opacity=".16" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linejoin="round"/><path d="M12 16h.008M12 8v5" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/></svg><small class="mx-auto text-center text-danger">Something went wrong. Check your connection and try again</div>';
-	});
 
 //make navbar transparent when user scrolls down.
 var main_nav = document.getElementById("main-nav");
