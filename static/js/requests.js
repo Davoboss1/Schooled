e29d58e97aadeacf13148447bd81ad455b7b3b90
Davo <davidakinfenwa2@gmail.com>
2023-01-27 07:59:07 +0100

    ..

--- static/js/requests.js
+++ static/js/requests.js
@@ -23,9 +23,12 @@ document.addEventListener("htmx:responseError", function (event) {
     let element = detail.elt;
     let target = detail.target;
     let message = '<div class="alert alert-danger mx-auto alert-dismissible fade show" role="alert"><strong>Sorry!</strong>  But your request could not be completed. <hr class="mb-0">' + detail.xhr.responseText + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
-
-    if (element.hasAttribute("hx-error-target"))
+    console.log(detail);
+    if (element.hasAttribute("hx-error-target")){
+        console.log("Has error attr")
+        console.log(message)
         document.querySelector(element.getAttribute("hx-error-target")).innerHTML = message;
+    }
     else
         target.innerHTML = message;
 
@@ -35,7 +38,7 @@ document.addEventListener("htmx:sendError", function (event) {
     let detail = event.detail;
     let element = detail.elt;
     let target = detail.target;
-
+    console.log("Response Error")
     let message = '<div class="d-flex flex-column h-100 justify-content-center"><svg class="mx-auto" width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.23 7.913 7.91 3.23c.15-.15.35-.23.57-.23h7.05c.21 0 .42.08.57.23l4.67 4.673c.15.15.23.35.23.57v7.054c0 .21-.08.42-.23.57L16.1 20.77c-.15.15-.35.23-.57.23H8.47a.81.81 0 0 1-.57-.23l-4.67-4.673a.793.793 0 0 1-.23-.57V8.473c0-.21.08-.42.23-.57v.01Z" fill="#000000" fill-opacity=".16" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linejoin="round"/><path d="M12 16h.008M12 8v5" stroke="#d50000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/></svg><small class="mx-auto text-center text-danger">Something went wrong. Check your connection and try again</div>';
     
     if (element.hasAttribute("hx-error-target"))
@@ -49,6 +52,7 @@ document.addEventListener("htmx:afterRequest", function (event) {
     let detail = event.detail;
     let element = detail.elt;
     let tag_type = element.tagName.toLowerCase();
+    console.log("Response Error")
 
     if (detail.successful && tag_type === "form" && !element.hasAttribute("no-reset")) {
         detail.elt.reset();
